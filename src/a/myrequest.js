63768c139c05547dfed1e0f4e4034c556279d477
JOEMAND985 <djordjemandic85@gmail.com>
2018-07-20 01:23:33 +0200

    add https instead of http

--- src/a/myrequest.js
+++ src/a/myrequest.js
@@ -8,58 +8,53 @@ class MyRequest extends Component{
 			a: 'bla',
 			location: ''
 		}
-
-		 // this.bla = this.bla.bind(this)
-   //   this.update = this.update.bind(this)
+    this.update = this.update.bind(this)
+    this.show = this.show.bind(this)
 	}
 
+show(){
+  alert('show')
+}
 
-// var update = function bla(){
-// 	alert "halsdhffh"
-// }
-
-
-// update(){
-//   //alert("bbb")
-//   return 'blabla'
-// }
-
-componentDidMount(){
-
-// Geolocation
-if ("geolocation" in navigator) {
-   console.log('1. we have geolocation')
+update(){
+     console.log('1. we have geolocation')
 
   navigator.geolocation.getCurrentPosition(function(position){
 
-  const locUrl = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=LrltGW8HIbhBXo6GZsMLJP08tUGdJ3nT&q='+ position.coords.latitude+'%2C'+ position.coords.longitude;
+  const locUrl = 'https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=LrltGW8HIbhBXo6GZsMLJP08tUGdJ3nT&q='+ position.coords.latitude+'%2C'+ position.coords.longitude;
    console.log(locUrl) 
 
   fetch(locUrl)
   .then(function(response){
-  	return response.json()
+    return response.json()
   })
   .then(response => response.Key)
-  .then(loc => fetch('http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/' + loc +'?apikey=LrltGW8HIbhBXo6GZsMLJP08tUGdJ3nT&details=true&metric=true'))
+  .then(loc => fetch('https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/' + loc +'?apikey=LrltGW8HIbhBXo6GZsMLJP08tUGdJ3nT&details=true&metric=true'))
     .then(function(response) {
     return response.json();
   })
   .then(function(response){
     console.log(response)
-  	return response;
+  // this.show()
+    //this.setState({a: 'lalalab'})
+    //return response;
 })
-
 })
+}
 
 
-// debug end
+componentDidMount(){
+
+// Geolocation
+if ("geolocation" in navigator) {
+  this.update()
+  this.show()
 } else {
   alert("You must have geolocation activated!!!")
 }
-
-
-
 }
+
+
 
 	render(){
 	return  <div> {this.state.a} </div>
